09f96e4c80220ebf01b194c051a06a3a69be16a6
Stiven Bautista <msbautista2001@gmail.com>
2023-03-14 22:20:26 -0500

    Solve "Java Pattern Factory" problem

---
+++ Java/JavaFactoryPattern.java
@@ -0,0 +1,43 @@
+interface Food {
+    public String getType();
+}
+
+class Pizza implements Food {
+    public String getType() {
+        return "Someone ordered a Fast Food!";
+    }
+}
+
+class Cake implements Food {
+
+    public String getType() {
+        return "Someone ordered a Dessert!";
+    }
+}
+
+class FoodFactory {
+    public Food getFood(String order) {
+        switch (order) {
+            case "pizza":
+                return new Pizza();
+            case "cake":
+                return new Cake();
+            default:
+                return null;
+        }
+    }//End of getFood method
+
+}//End of factory class
+
+public class JavaFactoryPattern {
+
+    public static void main(String args[]) {
+        //creating the
+        FoodFactory foodFactory = new FoodFactory();
+        //factory instantiates an object
+        Food food = foodFactory.getFood("cake");
+        System.out.println("The factory returned " + food.getClass());
+        System.out.println(food.getType());
+    }
+
+}
