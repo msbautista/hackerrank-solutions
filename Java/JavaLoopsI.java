7e361c69bb7c14bac3b72eb573669a8bbab2ae63
Stiven Bautista <msbautista2001@gmail.com>
2023-03-23 22:52:10 -0500

    Solve "Java Loops I" problem

---
+++ Java/JavaLoopsI.java
@@ -0,0 +1,15 @@
+public class JavaLoopsI {
+
+    public static void main(String[] args) {
+        int number = 2;
+        printMultiples(number);
+    }
+
+    private static void printMultiples(int number) {
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.printf("%d x %d = %d%n", number, i, result);
+        }
+    }
+
+}
